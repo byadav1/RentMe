e38c1e76f7f0d7079999e63ffb40006033ef41cd
brandond-westga <bd00051@my.westga.edu>
2022-03-23 13:05:11 -0500

    Created connection to cs6232-g3 DB and added validation for login

---
+++ Model/Employee.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+
+namespace RentMe.Model
+{
+    /// <summary>
+    /// This class models a RentMe employee
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Employee attributes.
+        /// </summary>
+        public int EmployeeID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Phone { get; set; }
+        public string Sex { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Username { get; set; }
+        public string Type { get; set; }
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 0-param constructor.
+        /// </summary>
+        public Employee()
+        {
+
+        }
+    }
+}
