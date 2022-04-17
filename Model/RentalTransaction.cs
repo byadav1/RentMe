8b332d44a772e0c7b085e9be23c383ff37c3a4ca
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-04-17 14:39:52 +0100

    Added RentalTransaction Model

---
+++ Model/RentalTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentMe.Model
+{
+    /// <summary>
+    /// This class models a Rental transaction.
+    /// </summary>
+    public class RentalTransaction
+    {
+        /// <summary>
+        /// Rental transaction properties
+        /// </summary>
+        public int TransactionID { get; set; }
+        public int RentedItemsID { get; set; }
+        public int FurnitureID { get; set; }
+        public int Quantity { get; set; }
+        public int EmployeeID { get; set; }
+        public int MemberID { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime RentDate { get; set; }
+        public string FurnitureName { get; set; }
+        public string Category { get; set; }
+        public string Style { get; set; }
+        public string Description { get; set; }
+        public float RentalRate { get; set; }
+    }
+}
