76fa9b5abe1b6ebac7d5a0dd1b2f6306d8de08b1
Chijioke Amaefuna <ca00077@my.westga.edu>
2022-04-25 00:07:32 +0100

    Refactor password hash function, controller now call this function

---
+++ Controller/PasswordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentMe.Crypto;
+
+namespace RentMe.Controller
+{
+    public class PasswordController
+    {
+        public string GetSha256Hash(string password)
+        {
+            return PasswordHash.GetSha256Hash(password);
+        }
+    }
+}
