f7270583ab980723fb7b324e7c67e6da3664f93f
Chijioke Amaefuna <ca00077@my.westga.edu>
2022-04-25 00:11:14 +0100

    Code style corrections

--- Controller/PasswordController.cs
+++ Controller/PasswordController.cs
@@ -7,8 +7,18 @@ using RentMe.Crypto;
 
 namespace RentMe.Controller
 {
+    /// <summary>
+    /// This class serves as the controller,
+    /// mediator between the RentMe application
+    /// and the PasswordHash class.
+    /// </summary>
     public class PasswordController
     {
+        /// <summary>
+        /// Return sha256 hash of a string 
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>sha256 hash string</returns>
         public string GetSha256Hash(string password)
         {
             return PasswordHash.GetSha256Hash(password);
